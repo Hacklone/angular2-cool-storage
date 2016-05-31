dc12a3652986c118c00be8f25c5456aa71ddb96f
Barna Tóth <toakak@gmail.com>
2016-05-31 14:19:20 +0200

    Fix provider casing

--- cool-storage.ts
+++ cool-storage.ts
@@ -3,7 +3,7 @@ export { CoolLocalStorage } from "./src/cool-local-storage";
 import { CoolSessionStorage } from "./src/cool-session-storage";
 export { CoolSessionStorage } from "./src/cool-session-storage";
 
-export const Cool_STORAGE_PROVIDERS: any[] = [
+export const COOL_STORAGE_PROVIDERS: any[] = [
     CoolLocalStorage,
     CoolSessionStorage    
 ];
