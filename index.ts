0c7c4fcc0d78beaf3039f52ebb9d3d8a50e9d7a1
Barna Toth <Barna.Toth@logmein.com>
2016-12-12 11:32:29 +0100

    Add AOT support

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-export * from './cool-storage';
+import { NgModule } from '@angular/core';
+
+import { CoolLocalStorage } from "./src/cool-local-storage";
+export { CoolLocalStorage } from "./src/cool-local-storage";
+import { CoolSessionStorage } from "./src/cool-session-storage";
+export { CoolSessionStorage } from "./src/cool-session-storage";
+
+@NgModule({
+  providers: [CoolLocalStorage, CoolSessionStorage]
+})
+export class CoolStorageModule {
+}
