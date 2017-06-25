fe9cd7f2d1727fabc16d786290515a92fd1131e7
Nick van der Meij <nick.vander.meij@outlook.com>
2017-06-25 16:45:00 +0200

    Implemented browser check to catch 'window is not defined' exception in Angular Universal

--- src/cool-local-storage.ts
+++ src/cool-local-storage.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CoolStorageBase } from './cool-storage-base'
+import { CoolStorageBase } from './cool-storage-base';
+
+/**
+ * PLATFORM_ID and isPlatformBrowser to determine if the window object exists in current context
+ */
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable()
 export class CoolLocalStorage extends CoolStorageBase {
-  constructor() {
-    super(window.localStorage, 'LocalStorage');
+  constructor(@Inject(PLATFORM_ID) platformId: string) {
+      super((isPlatformBrowser(platformId) ? window.localStorage : [], 'LocalStorage');
   }
 }
